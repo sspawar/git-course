65ae1bcc4c21c8cad22b237446fe68d15e0a9a9b
sspawar <swapneel2025@gmail.com>
2024-11-13 21:57:23 -0500

    story 3.1 -user input

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i value is = " + i);
+            System.out.println(" Take input from user and add the values " );
         }
     }
 }
